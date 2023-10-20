ccea2e9cd4a21df3be8386ad021d1600ef83b435
Jules <jules.deschamps.dev@gmail.com>
2023-10-20 09:46:45 +0200

    refractoring code

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -13,16 +13,77 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeModalBtn = document.querySelector("span.close");
 
+// clode modal event
 closeModalBtn.addEventListener("click", closeModal)
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+/**
+ *  launch modal form
+ */
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+/**
+ * Ferme la modale
+ */
 function closeModal() {
   modalbg.style.display = "none";
+}
+
+
+
+
+
+
+/**
+ * controlleur du formulaire d'inscription
+ */
+const validate = (event) => {
+  event.preventDefault();
+  const target = event.target;
+
+  // renitialise les messages d'erreurs
+  let isError = false;
+  const errorMessages = document.querySelectorAll('.error-message');
+  errorMessages.forEach((element) => {
+    element.remove();
+  });
+
+  // recupération des valeurs du formulaire
+  const data = new FormData();
+  data.append('prenom', target.querySelector("input#first").value);
+  data.append('nom', target.querySelector("input#last").value);
+  data.append('email', target.querySelector("input#email").value);
+  data.append('anniversaire', target.querySelector("input#birthdate").value);
+  data.append('nbTournoi', target.querySelector("input#quantity").value);
+  data.append('localisation', target.querySelector('input[name="location"]:checked')?.value);
+  data.append('terms', target.querySelector("input[name='terms']").checked);
+  data.append('newsletter', target.querySelector("input#checkbox2").checked);
+
+  // controle des saisies du formulaire
+  for (const [key, value] of data.entries()) {
+    console.log(key, value)
+    // si la valeur necessite un controle et si la condition contenu dans les règles pour cette clef est rempli
+    if (errorsMessages.hasOwnProperty(key) && (formRules[key](value))) {
+      printFormErrors(`#formData${capitalizeFirstLetter(key)}`, errorsMessages[key]);
+    }
+  }
+}
+
+
+/**
+ * Affiche les messages d'erreurs dans le formulaire
+ * @param {string} selector selecteur html
+ * @param {string} message message à afficher
+ */
+const printFormErrors = (selector, message) => {
+  const errorMessage = document.createElement("p");
+  errorMessage.innerHTML = message;
+  errorMessage.classList.add('error-message');
+  console.log(selector);
+  document.querySelector(selector).appendChild(errorMessage);
+  isError = true;
 }
---
+++ starterOnly/js/utils.js
@@ -0,0 +1,30 @@
+// messages d'erreurs
+const errorsMessages = {
+  localisation: "Vous devez choisir une option. <br/><br/>",
+  prenom: "Le prénom doit faire au moins 2 caractères.",
+  nom: "Le nom doit faire au moins 2 caractères.",
+  email: "Vous devez saisir une adresse mail valide.",
+  anniversaire: "Vous devez avoir au moins 18ans.",
+  terms: "Vous devez accepter les conditions d'utilisation.",
+};
+
+const formRules = {
+  localisation: (value) => value === 'undefined',
+  prenom: (value) => value.length < 2,
+  nom: (value) => value.length < 2,
+  email: (value) => !/^[\w\.-]+@[\w\.-]+\.\w+$/.test(value),
+  anniversaire: (value) => !verifierAge(value),
+  terms: (value) => value === "false",
+};
+
+function verifierAge(date){
+const dateActuelle = new Date();
+const dateFournie = new Date(date);
+const majorite = 18 * 365.25 * 24 * 60 * 60 * 1000;
+return dateActuelle - dateFournie >= majorite;
+
+}
+
+function capitalizeFirstLetter(input) {
+  return input.charAt(0).toUpperCase() + input.slice(1);
+}
